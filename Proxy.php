c2167fa88f3586c1cc36f818b965d5c0180eeb4b
symfonyaml <symfonyaml@users.noreply.github.com>
2018-04-19 09:54:29 +1000

    Add files via upload

---
+++ Proxy.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace GetRepo\ProxyBundle;
+
+use SahusoftCom\ProxyChecker\ProxyCheckerService;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+/**
+ * Proxy.
+ */
+class Proxy
+{
+    /**
+     * @var array
+     */
+    private $ip;
+
+    /**
+     * @var \Symfony\Component\PropertyAccess\PropertyAccessor
+     */
+    private $port;
+
+    /**
+     * @param array $config
+     */
+    public function __construct($ip, $port)
+    {
+        $this->ip = $ip;
+        $this->port = $port;
+    }
+
+    public function __toString()
+    {
+        return "{$this->ip}:{$this->port}";
+    }
+
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    public function check($timeout = 0)
+    {
+        $proxy = (string) $this;
+        $proxyCheckObject = new ProxyCheckerService(
+            'http://captive.apple.com/',
+            ['timeout' => $timeout]
+        );
+        $result = $proxyCheckObject->checkProxies([$proxy]);
+
+        if ($result && isset($result[$proxy]['info'])) {
+            return $result;
+        }
+
+        return false;
+    }
+
+    public function call($url)
+    {
+        // TODO use guzzle to get response
+    }
+}
